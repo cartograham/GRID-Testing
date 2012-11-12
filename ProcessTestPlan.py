74c73f64cf58b4ad05be74969856b37325db40fc
James K Graham <volite@gmail.com>
2012-11-12 13:57:29 -0600

    Fixed comma-delimiting issue
    Added loop to inspect each column in the csv and remove commas

--- ProcessTestPlan.py
+++ ProcessTestPlan.py
@@ -1,4 +1,5 @@
 import csv, os, sys
+from pyparsing import commaSeparatedList
 
 def dict_filter(it, *keys):
     for d in it:
@@ -12,7 +13,7 @@ results_testplanfile = os.path.join(thisdir, results_file)
 
 #the output file assembles some of the original testplan info and merges it into the test plan results csv...
 #this single file includes the original questions, Ids and expected results with the test plan responses for each test taker.
-output_file = os.path.join(thisdir,orig_file.split(".")[0] + "_results." + orig_file.split(".")[1])
+output_file = os.path.join(thisdir,"output",orig_file.split(".")[0] + "_results." + orig_file.split(".")[1])
 
 #build list of headers in results
 with open(results_testplanfile,'rb') as results:
@@ -27,7 +28,7 @@ origheaders = ['Procedure_ID','Test_Procedure','Expected_Results']
 
 #--------------------
 #Remove extraneous data columns from test results csv.
-results_filteredtestplanfile = os.path.join(thisdir,orig_file.split(".")[0] + "_filter." + orig_file.split(".")[1])
+results_filteredtestplanfile = os.path.join(thisdir,"output",orig_file.split(".")[0] + "_filter." + orig_file.split(".")[1])
 
 f_in = open(results_testplanfile,'rb')
 f_out = open(results_filteredtestplanfile,'wb')
@@ -39,9 +40,20 @@ for line in reader:
     writer.writerow(line)
 
 #transpose data in filtered test results csv, convert to list...
+results_transposedtestplanfile = os.path.join(thisdir,"output",orig_file.split(".")[0] + "_transposed." + orig_file.split(".")[1])
 f = csv.reader(open(results_filteredtestplanfile))
 f_transposed = zip(*f)
+f_transposed_list = []
 count = len(f_transposed)
+#Remove non-delimiting commas from the mix.
+for item in f_transposed:
+    thisrow = list(item)
+    newrow = []
+    for value in thisrow:
+        newrow.append(value.replace(",", "- ")) #in case users use commas, we won't upset the comma-delimted format.
+    f_transposed_list.append(newrow)
+    
+
 
 #Open output csv file...
 f_output = open(output_file, 'wb')
@@ -51,8 +63,8 @@ testplan_lines = f_testplan.readlines()
 
 i=0
 while i < count:
-    #Write the original file info (ID,Procedure,Results) then the test results (Name, James, Aja, Travis, etc).  The number of lines should match (skip the 
-    f_output.write(testplan_lines[i].strip() + "," + ",".join(f_transposed[i][1:]) + "\n")
+    #Write the original file info (ID,Procedure,Results) then the test results (Name, James, Aja, Travis, etc).  The number of lines should match (skip the
+    f_output.write(testplan_lines[i].strip() + "," + ",".join(f_transposed_list[i][1:]) + "\n")
     i += 1
 
 f_output.close()
